dbd84864b9653cc253910430bbb2154348217ec3
Richard Levente Hodi <88036014+Sterbike@users.noreply.github.com>
2023-11-08 11:55:40 +0100

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import './App.css';
+import Login from './pages/Login/Login';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+          <Routes>
+              <Route path='/' element={<Login />} /> 
+              <Route></Route>
+              <Route></Route>
+              <Route></Route>
+              <Route></Route>
+              <Route></Route>
+          </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
